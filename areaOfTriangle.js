ece904688a267ccc40cb7a2cd65d7aedafe8cb85
Liman Halder <118035789+limon158@users.noreply.github.com>
2023-02-06 17:58:55 +0600

    Add files via upload

---
+++ areaOfTriangle.js
@@ -0,0 +1,11 @@
+// area of triangle
+function getArea(a, b, c)
+{
+    const area = 0.5*(a+b+c);
+    return area;
+}
+const a = 5;
+const b = 6;
+const c = 5;
+const area = getArea(a, b, c);
+console.log(area);
